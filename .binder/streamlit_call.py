c331bb3ed3f6c55d6a949a7f04dc09f7d236815e
Sergio Sánchez Zavala <chekos.data@gmail.com>
2020-01-07 08:53:13 -0800

    Update streamlit_call.py

--- .binder/streamlit_call.py
+++ .binder/streamlit_call.py
@@ -1,6 +1,5 @@
 from subprocess import Popen
 
-
 def load_jupyter_server_extension(nbapp):
-    """serve the bokeh-app directory with bokeh server"""
-    Popen(["streamlit", "hello", "--allow-websocket-origin=*", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
+    """serve the streamlit app"""
+    Popen(["streamlit", "hello", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
